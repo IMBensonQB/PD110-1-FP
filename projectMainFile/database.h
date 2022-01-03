ce1cfb9604f048fb50d4b1b2e92f9440f7638ee1
kevlius <liu635588@gmail.com>
2022-01-03 21:41:17 +0800

    Add function structrure of dbsearch

--- projectMainFile/database.h
+++ projectMainFile/database.h
@@ -8,6 +8,9 @@
 #include <QStandardItemModel>
 #include <QStandardItem>
 #include <QApplication>
+#include <functional>
+using namespace std;
+const int CATLEN = 8;
 const QString path = "C:\\Users\\user\\Documents\\GitHub\\PD110-1-FP\\projectMainFile\\Database\\course.db";
 QSqlDatabase getDatabaseConnection()
 {
@@ -21,6 +24,33 @@ QSqlDatabase getDatabaseConnection()
     QSqlQuery query;
     query.exec("INSERT into course (name) values ('test')");
     */
+}
+
+typedef QString Queven;
+
+QString sqlQueryMaker(bool* chooseCat, QString searchText, QString searchTextType, QVector<int> timeVec, QVector<QString> categories)
+{
+    QString query = "select * from course where";
+    for (int i = 0; i < CATLEN; i++)
+    {
+
+    }//chooseCat
+    switch (hash<QString>{}(searchTextType))
+    {
+
+    }//search
+    int vecLen = timeVec.length();
+    for (int i = 0; i < vecLen; i++)
+    {
+
+    }
+    vecLen = categories.length();
+    for (int i = 0; i < vecLen; i++)
+    {
+
+    }
+    return query;
+
 }
 
 #endif // DATABASE_H

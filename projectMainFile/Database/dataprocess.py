31de9e4378df366f31ccda4f8e7566fcfc3651e1
kevlius <33776151+kevlius@users.noreply.github.com>
2022-01-04 11:40:13 +0800

    Update dataprocess.py
    update data handling programme

--- projectMainFile/Database/dataprocess.py
+++ projectMainFile/Database/dataprocess.py
@@ -4,36 +4,61 @@ import openpyxl
 import subprocess
 
 def sqlprompt(row):
-    result = f'''
+    result = '''
     insert into course
     (NULL,
-    {row[0]},
-    {row[1]},
-    {row[2]},
-    {row[3]},
-    {row[4]},
-    {row[5]},
-    {row[6]},
-    {row[7]},
-    {row[8]},
-    {row[9]},
-    {row[10]},
-    {row[11]},
-    {row[12]},
-    {row[13]},
-    {row[14]},
-    {row[15]},
-    {row[16]},
-    {row[17]},
-    {row[18]},
-    {row[19]},
-    {row[20]},
-    {row[21]},
-    {row[22]},
-    {row[23]},
-    {row[24]},
-    {row[25]},'''
-    
+    '''
+    clsromlst = [12,13,18,19,20,21]
+    clasromstr = [""]*6
+    for index in clsromlst:
+        if (len(row[index]) > 3):
+            clasromstr.append(row[index])
+    for i in range(0,12):
+        try:
+            temp = int(row[i])
+            result += str(temp) + ","
+        except:
+            result += "\"" + row[i] + "\"" + ","
+    for i in range(0, 2):
+        result +=  "\"" + clasromstr[i] + "\"" + ","
+    for i in range(14, 18):
+        try:
+            temp = int(row[i])
+            result += str(temp) + ","
+        except:
+            result += "\"" + row[i] + "\"" + ","
+    for i in range(2, 6):
+        result +=  "\"" + clasromstr[i] + "\"" + ","
+    for i in range(21, 26):
+        try:
+            temp = int(row[i])
+            result += str(temp) + ","
+        except:
+            result += "\"" + row[i] + "\"" + ","
+    timeList = getTimeList(row[15])
+    for i in range(0,20):
+        result += str(timeList[i]) + ","
+    if "限大一" in row[16]:
+        result += "1,"
+        if "限大二" in row[16]:
+            result += "2,"
+            if "限大三" in row[16]:
+                    result += "3,"
+                    if "限大四" in row[16]:
+                        result += "4,"
+                    else:
+                        result+= "0,"
+    if "限本系" in row[16]:
+        result += "true,"
+    else:
+        result += "false,"
+    result += input("Category1: 0 共同 1 通識 2 系上課") + ","
+    result += input("Category2: 0 共同 1 通識 2 系上課") + ","
+    if "初選不開放" in row[16]:
+        result += "true,"
+    else:
+        result += "false,"
+    #libcat
     result += ");"
     return result
 
@@ -50,5 +75,3 @@ sheet = workbook.worksheets[0]
 for row in sheet.iter_rows(min_row=1, max_col=sheet.max_col, max_row=sheet.max_row, values_only=True):
     cursor.exec(sqlprompt(row))
 db.commit()
-
-=
